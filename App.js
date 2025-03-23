e67dd632b2da4e208136b57a328b4c839b415e6a
Kristina Gordey <kristinababichvic@gmail.com>
2025-03-23 19:09:10 +0300

    created 2 pages

--- App.js
+++ App.js
@@ -1,13 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, SafeAreaView, TouchableOpacity } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import MenuScreen from './MenuScreen';
+
+const Stack = createStackNavigator();
+
+function LoginScreen({ navigation }) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = () => {
+        navigation.navigate("Menu", { username }); 
+    };
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.header}>Вход</Text>
+            
+            <TextInput
+                style={styles.input}
+                placeholder="Имя пользователя"
+                value={username}
+                onChangeText={setUsername}
+            />
+            
+            <TextInput
+                style={styles.input}
+                placeholder="Пароль"
+                secureTextEntry={true}
+                value={password}
+                onChangeText={setPassword}
+            />
+            
+            <TouchableOpacity style={styles.button} onPress={handleLogin}>
+                <Text style={styles.buttonText}>Войти</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    );
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Login">
+                <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="Menu" component={MenuScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +58,32 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#5fd1a5',
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    marginBottom: 15,
+    paddingHorizontal: 10,
+  },
+  button: {
+    backgroundColor: '#5fd1a5',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    width: '100%',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
   },
 });
---
+++ MenuScreen.js
@@ -0,0 +1,154 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+export default function MenuScreen({ route }) {
+    const { username } = route.params;
+    const [showMessage, setShowMessage] = useState(true); 
+    const [isDarkMode, setIsDarkMode] = useState(false); 
+    useEffect(() => {
+        
+        const timer = setTimeout(() => {
+            setShowMessage(false);
+        }, 3000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    const toggleTheme = () => {
+        setIsDarkMode((prevMode) => !prevMode); 
+    };
+
+    const styles = isDarkMode ? darkStyles : lightStyles; 
+
+    return (
+        <View style={styles.container}>
+            {showMessage ? (
+                <Text style={styles.message}>Добро пожаловать, {username}!</Text>
+            ) : (
+                <>
+                    <Text style={styles.header}>MyAdrenalin</Text>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>Мой профиль</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>Мои записи</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>История</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>Настройки</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>Контакты</Text>
+                    </TouchableOpacity>
+                </>
+            )}
+            <TouchableOpacity style={styles.themeButton} onPress={toggleTheme}>
+                <Text style={styles.themeButtonText}>
+                    {isDarkMode ? "Светлая тема" : "Тёмная тема"}
+                </Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const lightStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f8f8f8',
+    paddingHorizontal: 20,
+  },
+  message: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#5fd1a5',
+  },
+  header: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#5fd1a5',
+    marginBottom: 30,
+  },
+  button: {
+    backgroundColor: '#5fd1a5',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 5,
+    marginVertical: 10,
+    width: '80%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  themeButton: {
+    backgroundColor: '#ccc',
+    position: 'absolute', 
+    bottom: 20, 
+    right: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  themeButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+});
+
+const darkStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#333',
+    paddingHorizontal: 20,
+  },
+  message: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#5fd1a5',
+  },
+  header: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#5fd1a5',
+    marginBottom: 30,
+  },
+  button: {
+    backgroundColor: '#5fd1a5',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 5,
+    marginVertical: 10,
+    width: '80%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  themeButton: {
+    position: 'absolute', 
+    bottom: 20, 
+    right: 20, 
+    backgroundColor: '#555',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+},
+
+  themeButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+});
